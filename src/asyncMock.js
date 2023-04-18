03147c33dcd050e2e07b0d577b523b3f61e62dd2
Cristian Rodriguez <an1me@hotmail.es>
2023-04-18 01:06:41 -0500

    incersion stock

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -10,6 +10,15 @@ const products = [
     },
     {
         id: "2",
+        name: "Casco Kov Rush",
+        price: 950,
+        category: "Kov",
+        img: "https://s2r.mx/wp-content/uploads/2022/07/Diseno-sin-titulo-52.jpg",
+        stock: 19,
+        description: "CASCO KOV RUSH BRAINY AZUL - S2R Experience"
+    },
+    {
+        id: "3",
         name: "Bex Helmets",
         price: 300,
         category: "Bex",
@@ -18,14 +27,50 @@ const products = [
         description: "Casco para moto Bex Helmets Bx105"
     },
     {
-        id: "3",
+        id: "4",
         name: "Shark Raw Kubrik",
         price: 700,
         category: "Cafe Racer",
         img: "https://http2.mlstatic.com/casco-para-moto-shark-raw-D_NQ_NP_683021-MLM20700076226_052016-F.jpg",
         stock: 15,
         description: "Casco para moto Shark raw"
-    }
+    },
+    {
+        id: "5",
+        name: "Kov Rush Rosa",
+        price: 300,
+        category: "Kov",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS26O3lq21BNtI_7cS_Orba7DUnBW_w3pQ0SQ&usqp=CAU",
+        stock: 10,
+        description: "Casco Kov Rosa Fibra Carbono Abatible"
+    },
+    {
+        id: "6",
+        name: "Stealth Zapata Rojo",
+        price: 950,
+        category: "Kov",
+        img: "https://cdn.shopify.com/s/files/1/0097/1053/4713/products/zapatarojo3.png?v=1616468791",
+        stock: 19,
+        description: "nuevo chasis KOV Stealth con certificado DOT"
+    },
+    {
+        id: "7",
+        name: "Bex Arai",
+        price: 1000,
+        category: "Bex",
+        img: "https://http2.mlstatic.com/D_NQ_NP_706867-MCO44866947526_022021-O.webp",
+        stock: 12,
+        description: "Casco De Moto Color Amarillo-negro-rojo Talla S Marca Arai"
+    },
+    {
+        id: "8",
+        name: "Jet Chopper Harley",
+        price: 2000,
+        category: "Cafe Racer",
+        img: "https://http2.mlstatic.com/D_NQ_NP_837687-CBT50940738546_072022-O.jpg",
+        stock: 10,
+        description: "Casco 3/4 Piel Cafe Racer Jet Chopper Harley Rocket Vintage"
+    },
 
 ];
 
